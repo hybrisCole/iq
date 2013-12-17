9cd5da45a9e5682c1a5772ea4513a35fe4f8c6f8
Alberto Cole <acpii2005@gmail.com>
2013-12-16 20:23:49 -0600

    Limpiando este mierdero

--- app/scripts/controllers/IndexController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('sprintMobile.controllers').controller('IndexCtrl', ['$scope', 'PhoneFactory', function ($scope, PhoneFactory) {
-  $scope.slide = '';
-  $scope.phoneCategories = PhoneFactory.phoneCategories();
-  console.log($scope.phoneCategories);
-}]);
